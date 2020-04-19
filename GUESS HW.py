eaaa1846c27ec3bfd9b46c9ff25bc336ea6fd399
Ifeoluwa Adesina S <adesinaifeoluwa@rocketmail.com>
2020-04-18 22:03:49 -0400

    changes in grammer

--- GUESS HW.py
+++ GUESS HW.py
@@ -10,10 +10,10 @@ user_guess = None  # the initial value of user guess is None
 while user_guess != rand_no:  # the next lines o code will only run if the condition after the "while" remains true
     user_guess = int(input("Guess a number between 100 and 1000"))
     if user_guess > rand_no:
-        print("The number is higher than guess. Guess lower")
-
+        print("The number is higher than what you guessed. Guess lower")
+ you
     if user_guess < rand_no:
-        print("The number is lower than guess. Guess higher")
+        print("The number is lower than what you guessed. Guess higher")
 
     elif user_guess == rand_no:
         print("Good job. you guesssed correctly")
